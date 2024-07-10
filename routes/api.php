7de28dfa64a90289bad6d9cccb6c6e18b08e3a91
GowthamSelvaraj123 <gowthamdesigner94@gmail.com>
2024-07-10 07:45:13 +0530

    Api Successfully Installed

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,3 @@ use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::class);
 
-
-
-
-
-
--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{any}', function () {
+    return view('welcome'); 
+})->where('any', '.*');
+
+Route::get('/check-db-connection', function () {
+    if (DB::connection()->getPdo()) {
+        return "Database connection is successful.";
+    } else {
+        return "Could not connect to the database.";
+    }
 });
-
-// Route::get('/{any}', function () {
-//     return view('welcome'); 
-// })->where('any', '.*');
-
-// Route::get('/check-db-connection', function () {
-//     if (DB::connection()->getPdo()) {
-//         return "Database connection is successful.";
-//     } else {
-//         return "Could not connect to the database.";
-//     }
-// });
